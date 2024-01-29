ba40a2bef3f0f156a78a679d0ac4ce79c4e18509
mainxml <mainxml@gmail.com>
2024-01-29 17:36:49 +0800

    添加归并排序，但暂无动画

--- app/src/main/java/com/mainxml/visualgo/util/Algo.kt
+++ app/src/main/java/com/mainxml/visualgo/util/Algo.kt
@@ -1,5 +1,7 @@
 package com.mainxml.visualgo.util
 
+import java.util.Collections.swap
+
 /**
  * @author zcp
  */
@@ -120,7 +122,29 @@ object Algo {
 
     /**
      * 快速排序
-     * 关于方向问题：从基准对向开始
+     * ```
+     * 一种基于分治策略的排序算法。选择数组中的某个元素作为基准数，
+     * 将所有小于基准数的元素移动到其左侧，将所有大于基准数的元素移动到其右侧。
+     *
+     * 选取数组左端元素作为基准数，初始化两个指针 i 和 j 分别指向数组的两端。
+     * 从小到大排序的话：
+     * 1. 使用 j 从右向左寻找一个比基准数小的元素。
+     * 2. 使用 i 从左向右寻找一个比基准数大的元素。
+     * 3. 交换这两个位置的元素。
+     * 4. 重复上述步骤。
+     * 5. 当 i 和 j 重合时，这个位置的元素和基准数交换，并作为分割两个子数组的分界线。基准数已有序。
+     * 6. 对两个子数组递归上述步骤。
+     *
+     * 如果基准数不在最左端则交换到最左端再继续。
+     * 关于指针谁先开始问题：从基准数对向开始。
+     *
+     * 时间复杂度为 O(n log n)、自适应排序、非稳定排序。
+     * 最差复杂度为 O(n^2)，没有归并排序稳定，但出现最差情况的概率很低。
+     * ```
+     * @param a IntArray
+     * @param left Int
+     * @param right Int
+     * @param onSwap TwoIndexCallback
      */
     fun quickSort(a: IntArray, left: Int, right: Int, onSwap: TwoIndexCallback) {
         if (left >= right) {
@@ -131,11 +155,11 @@ object Algo {
         var j = right
 
         while (i < j) {
-            // 从右往左寻找小于基准值的下标
+            // 从右往左寻找小于基准数的下标
             while (i < j && a[j] >= a[left]) {
                 j--
             }
-            // 从左往右寻找大于基准值的下标
+            // 从左往右寻找大于基准数的下标
             while (i < j && a[i] <= a[left]) {
                 i++
             }
@@ -143,14 +167,61 @@ object Algo {
             swap(a, i, j)
             onSwap(i, j)
         }
-        // 此时 i == j，和基准位置交换，基准值归位
+        // 此时 i == j，和基准位置交换，基准数归位
         swap(a, left, i)
         onSwap(left, i)
 
-        // 对基准值左子数组排序
+        // 对基准数左子数组排序
         quickSort(a, left, i - 1, onSwap)
-        // 对基准值右子数组排序
+        // 对基准数右子数组排序
         quickSort(a, i + 1, right, onSwap)
+    }
+
+    /**
+     * 归并排序
+     * ```
+     * &_&
+     * ```
+     * @param a IntArray
+     * @param left Int
+     * @param right Int
+     */
+    fun mergeSort(a: IntArray, left: Int, right: Int) {
+        // 归 | 长度为 1 时终止
+        if (left >= right) {
+            return
+        }
+
+        // 递 | 递归二分切割
+        val mid = (left + right) / 2
+        mergeSort(a, left, mid)
+        mergeSort(a, mid + 1, right)
+
+        // 归 | 合并阶段（最先执行时长度为 2
+
+        // 准备一个临时数组来存储有序元素
+        val sorted = IntArray(right - left + 1)
+
+        // 在两个子数组中，将小的元素依次放入有序数组
+        var i = left
+        var j = mid + 1
+        var k = 0
+        while (i <= mid && j <= right) {
+            sorted[k++] = if (a[i] < a[j]) a[i++] else a[j++]
+        }
+        // 将左子数组剩余元素放入有序数组
+        while (i <= mid) {
+            sorted[k++] = a[i++]
+        }
+        // 将右子数组剩余元素放入有序数组
+        while (j <= right) {
+            sorted[k++] = a[j++]
+        }
+
+        // 有序数组对应回原数组
+        for (l in sorted.indices) {
+            a[left + l] = sorted[l]
+        }
     }
 
     private fun swap(a: IntArray, i: Int, j: Int) {
